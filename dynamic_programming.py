2f3fff1735ca6b9a52733ee3a413fb7bd54094f7
Alex-Yola <doc-1729@yandex.com>
2021-04-21 18:34:43 +0300

    Add track_counter(x, y)

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -26,7 +26,6 @@ def fib2(n):
         F0, F1 = F1, Fn
     return Fn
 
-
 def test_fib2(N):
     for n in range(N + 1):
         print('F' + str(n), '=', fib2(n))
@@ -35,7 +34,7 @@ test_fib2(10)
 
 
 def fib3(n):
-    """Возвращает массив чисел Фибоначчи для заданного порядка n >= 0 в цикле.
+    """Возвращает массив чисел Фибоначчи для заданного порядка n > 0 в цикле.
     Вариант с использованием массива F[0..n].
     """
     F = [0, 1] + [0] * (n - 1)
@@ -44,3 +43,25 @@ def fib3(n):
     return F
 
 print(fib3(10))
+
+
+def track_counter(x, y):
+    """Возвращает матрицу с числом путей к клеткам с координатами (0..x, 0..y).
+    Шаги: (0,1) или (1,0).
+    """
+    K = [[0] * (x + 1) for n in range(y + 1)]
+    for i in range(y + 1):
+        K[i][0] = 1
+    for j in range(x + 1):
+        K[0][j] = 1
+    for i in range(1, y + 1):
+        for j in range(1, x + 1):
+            K[i][j] = K[i - 1][j] + K[i][j - 1]
+    return K
+
+def test_track_counter(x, y):
+    K = track_counter(x, y)
+    for n in range(y + 1):
+        print(K[n])
+
+test_track_counter(3, 3)
