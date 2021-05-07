b5d510be936f428728f9ecd79f20621723e28eb8
Alex-Yola <doc-1729@yandex.com>
2021-05-07 13:09:13 +0300

    Вставка всего из N12... .py в N13

---
+++ N13_stack_algorithms.py
@@ -0,0 +1,61 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return self.items == []
+    def push(self, item):
+        self.items.append(item)
+    def pop(self):
+        return self.items.pop()
+    def peek(self):
+        return self.items[len(self.items) - 1]
+    def size(self):
+        return len(self.items)
+
+def test_Stack():
+    s = Stack()
+    print("s.size():", s.size())
+    print("s.isEmpty()", s.isEmpty())
+    s.push("a")
+    s.push("b")
+    s.push("c")
+    print("s.peek():", s.peek())
+    print("s.size():", s.size())
+    print("s.isEmpty():", s.isEmpty())
+    print("s.pop():", s.pop())
+    print("s.pop():", s.pop())
+    print("s.pop():", s.pop())
+    print("s.size():", s.size())
+    print("s.isEmpty():", s.isEmpty())
+
+test_Stack()
+
+
+def checking_braces(s):
+    """Проверка последовательности скобок на закрытие.
+    Возвращает соответственно либо True, либо Folse.
+    """
+    a = Stack()
+    for brace in s:
+        if brace not in "()[]":
+            continue
+        if brace in "([":
+            a.push(brace)
+        else:
+            if a.isEmpty():
+                return False
+            left = a.pop()
+            if left == "(":
+                right = ")"
+            elif left == "[":
+                right = "]"
+            if right != brace:
+                return False
+    return a.isEmpty()
+
+def test_checking_braces():
+    print(checking_braces("__(_[(-(_))]_)__"))
+    print(checking_braces("__)_[(-(_))]_)__"))
+    print(checking_braces("__(_[(-(_))]_))__"))
+
+test_checking_braces()
